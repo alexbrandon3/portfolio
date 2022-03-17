91315425d78c23aef131c637e69e25c605dc3098
Alexander Brandon <alexlbrandon@gmail.com>
2022-03-17 11:45:51 -0400

    added link to updated resume

--- src/App.js
+++ src/App.js
@@ -1,36 +1,30 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Navbar from './components/Navbar.js';
-import About from './components/About.js';
-import Work from './components/Work.js';
-import Contact from './components/Contact.js';
-import Resume from './components/Resume.js';
-import Footer from './components/Footer.js';
-
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import About from "./components/About.js";
+import Work from "./components/Work.js";
+import Contact from "./components/Contact.js";
+import Resume from "./components/Resume.js";
 
 function App() {
-  const [selectedTab, setSelectedTab] = useState('About');
+  const [selectedTab, setSelectedTab] = useState("About");
   const renderChosenTab = () => {
     switch (selectedTab) {
-      case 'Work':
-        return <Work/>
-      case 'Contact Me':
-        return <Contact/>
-      case 'Resume':
-        return <Resume/>
+      case "Work":
+        return <Work />;
+      case "Contact Me":
+        return <Contact />;
+      case "Resume":
+        return <Resume />;
       default:
-        return <About/>
-
+        return <About />;
     }
-  }
+  };
   return (
     <div className="App">
-      <Navbar selectedTab={selectedTab}
-      setSelectedTab={setSelectedTab}
-      />
+      <Navbar selectedTab={selectedTab} setSelectedTab={setSelectedTab} />
       {renderChosenTab()}
-      <Footer/>
     </div>
   );
 }
--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,14 @@
 import React from "react";
-import photo from "./images/photo.jpg"
+import photo from "./images/photo.jpg";
 export default function About() {
   return (
     <div className="container mx auto flex place-items-center flex-col">
-      <img className="image inline h-60 rounded-lg" src={photo} alt="Me in Alaska" />
-      <p className="w-1/3 my-4">My name is Alexander Brandon and I'm a full-stack developer out of North Carolina. My professional interests include building exciting applications which will make life simpler for people. When I am not coding, I enjoy outdoor activities like fishing and boating. I am also in the process of recovering from four strokes of unknown origin which were originally projected to leave me as a quadriplegic for life.</p>
+      <a
+        href="https://alexander-brandon.herokuapp.com/"
+        className="flex p-4 border-2 h-48 justify-center items-center rounded-xl bg-blue-600"
+      >
+        Please click here to navigate to my updated portfolio!
+      </a>
     </div>
   );
 }
